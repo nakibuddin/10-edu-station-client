abb9743ce20ed5ac38768d23eaf480125b33627f
nakibuddin <nakibaust137@gmail.com>
2022-10-26 18:02:42 +0600

    Login page added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 
 import Main from './layout/Main/Main';
 import PageNotFound from './components/PageNotFound/PageNotFound';
+import Login from './components/Login/Login';
 
 
 const my_router = createBrowserRouter([
     {path: '/', element: <Main></Main>, children: [
         {path: '/home', element: <>This is home</>},
+        {path: 'login', element: <Login></Login>},
         {path: '*', element: <PageNotFound></PageNotFound>},
 
     ]}
---
+++ src/components/Login/Login.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import './Login.css'
+import login_img from './../../images/login.PNG'
+
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    const success = 'success';
+    const handleRegister = () => {        
+    }
+    const passwordError = 'password Error';
+    return (
+        <div className='display_grid'>
+            <div className='w-75 mx-auto'>                
+                <img className='img-fluid h-100 hide' src={login_img} alt="" />
+            </div>
+
+            <div>
+                <div className='mx-auto login'>
+                    <h3 className='mb-4 my_color'>Good to see you again!</h3>
+
+
+                    <Form onSubmit={handleRegister}>                    
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email address</Form.Label>
+                            <Form.Control type="email" name="my_email" placeholder="Enter email" required/>                    
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Password</Form.Label>
+                            <Form.Control type="password" name="my_password" placeholder="Password" required/>
+                        </Form.Group>
+                        
+                        <div className='text-center mt-4'>
+                            <Button variant="primary w-75" type="submit">
+                                Log in
+                            </Button>                
+                        </div>
+                    </Form>
+
+
+                    <div className='text-center'>
+                        {/* <p className='mt-3'><small>New to this website? Please <Link to='/register'>Register</Link></small></p> */}
+                        {/* <p className='mt-3'><small>Don’t have a account? <Link to='/register'>Register now</Link></small></p> */}
+                        <p className='mt-3 fw-semibold'><small>Don’t have a account? <Link to='/register'>Create an account</Link></small></p>
+
+                        <button type="button" className="btn btn-outline-primary w-75 mt-3 mb-3">Continue with google</button> <br/>
+                        <button type="button" className="btn btn-outline-dark w-75">Continue with github</button>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
