da05c9731597ba813846d20ad7918ccb79213a2f
nakibuddin <nakibaust137@gmail.com>
2022-10-26 17:59:59 +0600

    Page not found added

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 
 import Main from './layout/Main/Main';
+import PageNotFound from './components/PageNotFound/PageNotFound';
 
 
 const my_router = createBrowserRouter([
     {path: '/', element: <Main></Main>, children: [
         {path: '/home', element: <>This is home</>},
+        {path: '*', element: <PageNotFound></PageNotFound>},
 
     ]}
 ])
